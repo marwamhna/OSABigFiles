ebe5a96bc3fe08db744fbf16b09163f2cdc4a332
Margarita <45617576+margaritalm@users.noreply.github.com>
2019-02-19 10:43:08 +0200

    Add files via upload
    rr

---
+++ teamCityPoc/archives_examples/Login_jsp.java
@@ -0,0 +1,33 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import org.apache.jasper.runtime.*;
+
+public class Login_jsp extends HttpJspBase {
+
+
+//
+//   Filename: Common.jsp
+//   Generated with CodeCharge  v.1.2.0
+//   JSP.ccp build 05/21/2001
+//
+
+  static final String CRLF = "\r\n";
+
+  static final int UNDEFINT=Integer.MIN_VALUE;
+
+  static final int adText = 1;
+  static final int adDate = 2;
+  static final int adNumber = 3;
+  static final int adSearch_ = 4;
+  static final int ad_Search_ = 5;
+  static final String appPath   ="/";
+
+//Database connection string
+
+  static final String DBDriver  ="";
+  static final String strConn   ="";
+  static final String DBusername="";
+  static final String DBpassword="";
+
+}
